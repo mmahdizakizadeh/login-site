6aab2cd637b6774c271d795bee6273081af782c5
Mmahdi Zakizadeh <m.mahdi.zakizadeh.1380@gmail.com>
2026-08-24 20:08:39 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 const log = document.getElementById("login");
 const reg = document.getElementById("register");
 const btn = document.getElementById("btn");
-
-// هنگامی که روی دکمه ورود بزنیم
+
 function login() {
   log.style.right = "0px";
   reg.style.right = "450px";
   btn.style.right = "0px";
-}
-
-// هنگامی که روی دکمهثبت نام بزنیم
+}
 function register() {
   log.style.right = "-450px";
   reg.style.right = "0px";
